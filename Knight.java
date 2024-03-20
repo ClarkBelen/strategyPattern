3e0dfa923ce9b22ee1931dab4429ba103229bcb5
Clark Belen <142368338+ClarkBelen@users.noreply.github.com>
2024-03-21 01:13:19 +0800

    Update Knight.java

--- Knight.java
+++ Knight.java
@@ -1,20 +1,20 @@
 public class Knight implements CharacterType{
     public void attack(){
         System.out.print("Knight ");
-        new SwingSword().useAttackStrategy();;
+        new SwingSword().useAttackStrategy();
         
     }
     public void defend(){
         System.out.print("Knight ");
-        new ShieldDefense().useDefendStrategy();;
+        new ShieldDefense().useDefendStrategy();
         
 
         System.out.print("Knight ");
-        new DodgeDefense().useDefendStrategy();;
+        new DodgeDefense().useDefendStrategy();
         
 
         System.out.print("Knight ");
-        new MagicDefense().useDefendStrategy();;
+        new MagicDefense().useDefendStrategy();
         
     }
 }
